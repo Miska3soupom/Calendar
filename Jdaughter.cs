5962407fa403711b228096002a52e4c4ed880e5c
Miska3soupom <112772104+Miska3soupom@users.noreply.github.com>
2023-10-04 14:38:03 +0300

    Final Round

---
+++ Jdaughter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace To_do_list_wpf
+{
+    internal class Jdaughter
+    {
+        public static void Serialize<T>(T zametki)
+        {
+            File.WriteAllText(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\zametki.json", JsonConvert.SerializeObject(zametki));
+        }
+        public static T Deserialize<T>()
+        {
+            if (!File.Exists((Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\zametki.json")))
+            {
+                File.Create(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\zametki.json");
+            }
+            var fils = File.ReadAllText(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\zametki.json");
+            return JsonConvert.DeserializeObject<T>(fils);
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace To_do_list_wpf
+{
+    public partial class MainWindow : Window
+    {
+        public List<Zametka> Zametki = new List<Zametka>();
+        public List<Zametka> DoToday = new List<Zametka>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            Zametki = Jdaughter.Deserialize();
+            Date.Text = DateTime.Now.ToString();
+        }
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.ListBox1.SelectedItems.Count != 0)
+            {
+                DoToday[ListBox1.SelectedIndex].name = Name.Text;
+                DoToday[ListBox1.SelectedIndex].description = Description.Text;
+                clear();
+                Name.Text = "";
+                Description.Text = "";
+            }
+            else MessageBox.Show("Не выбрана заметка.");
+        }
+
+        private void Create_Click(object sender, RoutedEventArgs e)
+        {
+            if (Name.Text != "" && Description.Text != "")
+            {
+                Zametka zametka = new Zametka(Name.Text, Description.Text, Convert.ToDateTime(Date.SelectedDate));
+                Zametki.Add(zametka);
+                clear();
+                Name.Text = "";
+                Description.Text = "";
+            }
+            else MessageBox.Show("Вы не ввели имя или описание заметки.");
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.ListBox1.SelectedItems.Count != 0)
+            {
+                Zametki.RemoveAt(Zametki.IndexOf(DoToday[ListBox1.SelectedIndex]));
+                clear();
+                Name.Text = "";
+                Description.Text = "";
+            }
+            else MessageBox.Show("Не выбрана заметка.");
+        }
+
+        private void ListBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ListBox1.SelectedIndex != -1)
+            {
+                Name.Text = DoToday[ListBox1.SelectedIndex].name;
+                Description.Text = DoToday[ListBox1.SelectedIndex].description;
+            }
+        }
+
+        private void Date_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            clear();
+        }
+        private void clear()
+        {
+            ListBox1.Items.Clear();
+            DoToday.Clear();
+            foreach (Zametka item in Zametki)
+            {
+                if (Convert.ToDateTime(Date.SelectedDate).Day == item.data.Day)
+                {
+                    ListBox1.Items.Add(item.name);
+                    DoToday.Add(item);
+                }
+            }
+            Jdaughter.Serialize(Zametki);
+        }
+    }
+}
